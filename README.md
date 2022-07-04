371ea599828459c66af20a2778fcc044b8d3e560
sky0014 <sky-wang@qq.com>
2022-07-04 17:53:28 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ${PROJECT_NAME}
+${PROJECT_DESCRIPTION}
+
+## Install
+```bash
+npm install ${PROJECT_NAME}
+```
+
+## Publish
+If your first time publish a package, login first:
+```bash
+npm login --registry=http://registry.npmjs.org
+```
+Then you can publish:
+```bash
+npm run pub
+```
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "lib",
+    },
+    external: [],
+    plugins: [typescript()],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "lib/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
